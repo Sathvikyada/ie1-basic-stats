cd4759d001aa63aa173178eff9217fe85ba27e26
prathik183 <suryaprathik18@gmail.com>
2024-10-06 12:21:16 -0400

    Min Max Calculations and Views

---
+++ src/Views/MinView.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import javax.swing.JTextField;
+import javax.swing.text.JTextComponent;
+
+
+public class MinView implements View {
+
+	//Instance variables
+	double min;
+	JTextField jtfMin;
+	String name;
+
+	public MinView() {
+		min = 0;
+		jtfMin = new JTextField(5);
+		jtfMin.setEditable(false);
+		name = "Minimum";
+	}
+
+	@Override
+	public void update(ArrayList<Double> numbers) {
+		min = MinMaxCalculation.minMax(BasicStats.getArrayDouble(numbers))[0];
+
+		//Set text
+		jtfMin.setText("" + min);
+
+	}
+
+	@Override
+	public void reset() {
+		jtfMin.setText("");
+	}
+
+	@Override
+	public JTextComponent show() {
+		return jtfMin;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	public double getMedian() {
+		return min;
+	}
+
+}
